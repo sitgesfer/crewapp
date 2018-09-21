3d2f38c8c0894653022254594f302f5d4608bda5
Fernando Rivas <ferrivas@gmail.com>
2018-09-21 11:49:27 +0200

    Set up initial React environment using webpack and babel. Also added eslint with airbnb ruleset.

---
+++ webpack.common.js
@@ -0,0 +1,73 @@
+// Common webpack config
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: [
+        '@babel/polyfill',
+        './src/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+/*            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },*/
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: '/styles/fonts/',
+                        name: '[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles/[name].css",
+        }),
+    ]
+};
---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+// Production webpack config
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+    ]
+});
